5c36aa1dcb36ea0a6d33fd68914b4a1ded11bf7b
phanhoangnhan91@gmail.com <phanhoangnhan91@gmail.com@5b5581a2-ba26-254c-4b83-d48d06d6ebbc>
2014-06-16 11:08:37 +0000

    commit all

---
+++ encrypt/trunk/encrypt/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace encrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string s=encrypt.EncryptStringAES("PhanHoangNhan");
+            //string a=encrypt.DecryptStringAES(s);
+            //doc file text
+
+            // ghi file text mahoa
+        }
+    }
+}
